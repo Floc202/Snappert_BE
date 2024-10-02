4f41a7d303c759cb488572e6a76c2d142ea423af
NguyenTuanLoc <locntse161090@fpt.edu.vn>
2024-10-03 02:37:08 +0700

    done GenericRepo and UnitOW

--- ProductManagement.Repository/GenericRepository.cs
+++ ProductManagement.Repository/GenericRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagement.Repository.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,74 @@ using System.Threading.Tasks;
 
 namespace ProductManagement.Repository
 {
-    internal class GenericRepository
+    public class GenericRepository<T> where T : class
     {
+        protected readonly SNAPPETContext _context;
+
+        public GenericRepository(SNAPPETContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<int> CreateAsync(T entity)
+        {
+            await _context.AddAsync(entity);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> UpdateAsync(T entity)
+        {
+            _context.Attach(entity).State = EntityState.Modified;
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> RemoveAsync(T entity)
+        {
+            _context.Remove(entity);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        #region Prepare Operations for Batch Actions
+
+        public void PrepareCreate(T entity)
+        {
+            _context.Add(entity);
+        }
+
+        public void PrepareUpdate(T entity)
+        {
+            _context.Attach(entity).State = EntityState.Modified;
+        }
+
+        public void PrepareRemove(T entity)
+        {
+            _context.Remove(entity);
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            try
+            {
+                return await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                // Handle or log error
+                throw new Exception("Error while saving data.", ex);
+            }
+        }
+
+        #endregion
     }
+
 }
